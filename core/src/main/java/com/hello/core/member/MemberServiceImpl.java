79634510ecd0c28883d22b042e1c70e3f214e474
AnWoosang <dntkd857@soongsil.ac.kr>
2023-06-21 16:21:13 +0900

    AppConfig 추가

--- core/src/main/java/com/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/com/hello/core/member/MemberServiceImpl.java
@@ -7,7 +7,11 @@ public class MemberServiceImpl implements MemberService {
 
     // join에서 save를 호출하면 다형성에 의해서 인터페이스의 save가 아니라 MemoryMemberRepository에서 save가 호출이 된다.
     // DIP를 잘 준수하고 있는가? 아니지 왜? MemberRepository는 추상화에 의존하는게 맞지만 생성객체인 MemoryMemberRepository는 구현체에 의존하게 된다.(DIP위반)
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- core/src/test/java/com/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/com/hello/core/member/MemberServiceTest.java
@@ -1,12 +1,19 @@
 package com.hello.core.member;
 
+import com.hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
 
+    @BeforeEach
+    void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
     // Test의 작성방법은 필수이다.
     @Test
     void join(){
@@ -15,7 +22,7 @@ public class MemberServiceTest {
 
         // when
         memberService.join(member);
-        Member findmember = memberService.findMember(2L);
+        Member findmember = memberService.findMember(1L);
 
         // then
         Assertions.assertThat(member).isEqualTo(findmember);
